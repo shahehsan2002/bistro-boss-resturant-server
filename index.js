54773602d4ca5c87d54a346c23c949bee8329e91
shahehsan2002 <shahehsan2002@.com>
2023-11-17 21:45:49 +0600

    carts post api created

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
         const menuCollection = client.db("bistroDb").collection("menu");
         const reviewsCollection = client.db("bistroDb").collection("reviews")
+        const cartCollection = client.db("bistroDb").collection("carts")
 
         app.get('/menu', async (req,res) =>{
             const result = await menuCollection.find().toArray();
@@ -38,6 +39,19 @@ async function run() {
 
         app.get('/reviews', async (req,res) =>{
             const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
+        // carts collection
+        app.get('/carts',async (req,res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const result= await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/carts', async(req,res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
             res.send(result);
         })
 
